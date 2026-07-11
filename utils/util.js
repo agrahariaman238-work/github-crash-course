32805c47dbae05c580b8ad54a46b53b5c8eec0c1
Aman Agrahari <agrahariaman238@gmail.com>
2026-07-11 19:28:43 +0530

    add mul div fns

--- utils/util.js
+++ utils/util.js
@@ -5,3 +5,14 @@ function add(a, b) {
 function subtract(a, b) {
     return a - b;
 }       
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
